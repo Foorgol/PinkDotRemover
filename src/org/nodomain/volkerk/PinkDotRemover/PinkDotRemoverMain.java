7c1d2e9a5eb04ba1d0391583255c0f0b4ab8977d
Volker Knollmann <>
2013-06-18 23:06:11 -0400

    Added a basic GUI which is invoked if no file on the command line is provided.
    The GUI is not yet "filled with life"...

--- src/org/nodomain/volkerk/PinkDotRemover/PinkDotRemoverMain.java
+++ src/org/nodomain/volkerk/PinkDotRemover/PinkDotRemoverMain.java
@@ -32,7 +32,8 @@ public class PinkDotRemoverMain {
         // we need at least one file
         if (fList.size() == 0)
         {
-            printHelp();
+            //printHelp();
+            doGUI();
             return;
         }
         
@@ -121,5 +122,39 @@ public class PinkDotRemoverMain {
         System.err.println("Usage:");
         System.err.println("java -jar PinkDotRemover.jar <file1.dng or dir1> <file2.dng or dir2> etc.");
         System.err.println();
+    }
+    
+    protected static void doGUI()
+    {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+
     }
 }
